7c8d0322fd660eb5a3dca9aeee24464d8a216927
Maryia Paliashchuk <maryia.paliashchuk@gmail.com>
2020-05-19 23:21:40 +0300

    Added createTask and addTask functions

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 let tasklist=document.querySelectorAll('.task-list');
 let quantity=document.querySelectorAll('.quantity');
+let createButton=document.querySelector('.create-button');
+let userForm=document.forms.taskform;
 
+createButton.addEventListener('click',addTask);
 
 for(let i=0;i<tasklist.length;i++){
     setQuantity(tasklist[i],quantity[i]);
 }
+
 function setQuantity(tasklist,quantity){
     let counter=tasklist.getElementsByTagName('li').length;
     let text=document.createTextNode(counter);
     quantity.append(text);   
 }
+
+function addTask(){
+    createElement(userForm.elements.taskname.value);
+}
+
+function createTask(userData){
+    let element=document.createElement('li');
+    let text=document.createTextNode(userData);
+    element.append(text);
+    tasklist[0].append(element);
+}
+
